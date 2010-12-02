7913cc6eb47221f0dce6b5456db1425c02f8a661
LB426 <LB426@tih.kuban.ru>
2010-12-02 11:53:47 +0300

    создание проекта Magfolio 2 декабря 2010 11:53

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magfolio::Application.config.secret_token = 'dc223cb9226fde465a2ca34a7d5d1e1464b3aa011feb70d095672bca5dc2ad48ecdc1da55eae32c0070d0a6eb6e227fe4241273d7ec94b43838eaf7e655bb884'
