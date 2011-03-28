514b6860110bc1ddf751c96cf28e4b17d3e3111f
LB426 <LB426@tih.kuban.ru>
2011-03-28 17:13:26 +0400

    начал работу над CSS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery.ajaxSetup({
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
+
+$(document).ready(function() {
+  $("#slide_listed").hide();
+  $('#signup').mouseenter(function() {
+    $("#slide_listed").show("slide", { direction: "up" }, 700);
+  });
+  $('#signup').mouseleave(function() {
+    $("#slide_listed").hide("slide", { direction: "up" }, 700);
+  });
+})
