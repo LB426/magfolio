d324442c87e406cfbcd6779e4d56894c498c7cd5
Andrey Seredin <odissey@rambler.ru>
2011-04-05 22:16:33 +0400

    поправил загрузку логотипа

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -41,38 +41,20 @@ $(document).ready(function() {
     event.stopPropagation();
   });
   $('input[type=file]#best_business_image').change(function(event){
-    var userFile = $(this).val();
-    //alert(userFile);
-    var max = 5000000 ;
-    var iframe = $( '<iframe name="iframe" id="iframe" class="hidden" src="about:none" />' );
-    $("#logo_upload").append( iframe );
-    $('#business_img_upload_form').attr( "action", "/signup/logoupload" );
-    $('#business_img_upload_form').attr( "method", "post" );
-    $('#business_img_upload_form').attr( "userfile", userFile );
-    $('#business_img_upload_form').attr( "MAX_FILE_SIZE", max );
-    $('#business_img_upload_form').attr( "enctype", "multipart/form-data" );
-    $('#business_img_upload_form').attr( "encoding", "multipart/form-data" );
-    $('#business_img_upload_form').attr( "target", "iframe" );
-    $('#business_img_upload_form').submit();
-    $("#iframe").load(
-      function(){
-        //iframeContents = $("iframe#logo_upload_iframe").contentDocument.body.innerHTML;
-        //$("div#logo_upload").html(iframeContents);
-      }
-    );
+		
     return false;
   });
 	$('input[type=file]#signup_logo').change(function(event){
 		var userFile = $(this).val();
 		//alert(document.cookie);
 		//alert($.cookie("_session_id"));
-		$("#upload_progress_bar").show();
+		$("#logo_upload_progress_bar").show();
 		$('#new_signup').attr( "target", "upload_iframe" );
 		$('#new_signup').submit();
 		$("#upload_iframe").load(function(){
-			$("#upload_progress_bar").hide();
-			signup_id = $('#upload_iframe').contents().find('#signup_id').html();
-			alert(signup_id);
+			//signup_id = $('#upload_iframe').contents().find('#signup_id').html();
+			//alert(signup_id);
+			$("#logo_upload_progress_bar").hide();
 		});
 		return false;
 	});
