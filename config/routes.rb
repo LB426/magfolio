94e7475822863cf055b263f4b23822929252be3b
LB426 <LB426@tih.kuban.ru>
2011-04-12 16:50:23 +0400

    добавил маршруты для аутентификации

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Magfolio::Application.routes.draw do
   
-  get "sessions/new"
-
-  get "users/new"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  resources :users
+  resources :sessions
 
   resources :locations
 
