f4db6175cec2d86a1cfda8876a5d50614f57110a
LB426 <LB426@tih.kuban.ru>
2011-05-17 17:48:25 +0400

    сделал восстановление пароля через email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,4 +50,6 @@ Magfolio::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'spknd.ru' }
 end
