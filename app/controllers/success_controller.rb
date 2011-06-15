418c3e8d6bcce538c402caf697015ffdca6cb95a
LB426 <LB426@tih.kuban.ru>
2011-06-15 11:24:10 +0400

    привожу в порядок деплой на Руби1.9.2

--- app/controllers/success_controller.rb
+++ app/controllers/success_controller.rb
@@ -1,6 +1,6 @@
 class SuccessController < ApplicationController
   def index
-    @title = "Истории успеха с каталогом Клевер"
+    @title = "Success story"
     @body_css_class = "perma success about"
   end
 end
